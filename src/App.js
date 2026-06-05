8bb8e53713c073aa620fc0b2a3aa85b6ba02cae1
Codewabuzar <158184953+Codewabuzar@users.noreply.github.com>
2026-06-05 14:54:13 +0530

    Refactor App component to export FiguringOutMedia_v2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,1 @@
-function App() {
-  return (
-    <div>
-      <h1>Figuring Out Media</h1>
-      <p>Website is working!</p>
-    </div>
-  );
-}
-
-export default App;
+export { default } from "./FiguringOutMedia_v2";
